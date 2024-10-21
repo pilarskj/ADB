1d51f64b2e1d9df1295ea59df51f2dba8369a5c8
pilarskj <julia.pilarski@gmail.com>
2024-10-21 17:28:38 +0200

    fix inference, test MTT

---
+++ test/test/adbp/MultiTypeTreeTest.java
@@ -0,0 +1,29 @@
+package test.adbp;
+
+import multitypetree.evolution.tree.MultiTypeNode;
+import multitypetree.evolution.tree.MultiTypeTreeFromNewick;
+import org.junit.jupiter.api.Test;
+
+
+public class MultiTypeTreeTest {
+
+    @Test
+    public void testMultiTypeTree() {
+
+        String newick = "(1[&state=0] : 1.5, 2[&state=1] : 0.5)[&state=0];";
+
+        MultiTypeTreeFromNewick mtt = new MultiTypeTreeFromNewick();
+        mtt.initByName(
+                "adjustTipHeights", false,
+                "value", newick,
+                "typeLabel", "state");
+
+        int n = mtt.getTypeSet().getNTypes();
+        int m = mtt.getNodeCount();
+
+        for (int i = 0; i < m; i++) {
+            MultiTypeNode node = (MultiTypeNode) mtt.getNode(i);
+            System.out.println(node.getNodeType());
+        }
+    }
+}
