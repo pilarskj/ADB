b64def5ca5eda1f59b99c256d38b11c821afdff1
pilarskj <julia.pilarski@gmail.com>
2024-11-08 14:58:59 +0100

    add ADBP simulators in R and Java

---
+++ R/simulate_adbp.R
@@ -0,0 +1,207 @@
+library(ape)
+library(treeio)
+library(dplyr)
+library(assertthat)
+library(ggtree) # for plotting
+
+
+#' Simulator of a phylogeny from an Age-Dependent Branching Process
+#' @param origin_time time of birth of the initial particle
+#' @param origin_type one of 0,...,n-1 where n is the number of types
+#' @param a vector of scale parameters per type
+#' @param b vector of shape parameters per type
+#' @param d vector of death probabilities per type
+#' @param Xsi_as matrix of asymetric type transition probabilities
+#' @param Xsi_s matrix of symetric type transition probabilities
+simulate_phylogeny <- function(origin_time, origin_type, a, b, d, Xsi_as, Xsi_s, rho, min_tips = 2) {
+  # assert that all inputs are correct
+  ntypes = length(a)
+  assert_that(all(c(length(b) == ntypes, length(d) == ntypes, 
+                    dim(Xsi_as) == c(ntypes, ntypes), dim(Xsi_s) == c(ntypes, ntypes),
+                    origin_time > 0, origin_type %in% c(0:ntypes),
+                    d >= 0, d < 1, rho > 0, rho <= 1)),
+              msg = 'The inputs do not have proper dimensions or values. Please check all parameters!')
+  assert_that(all(sapply(seq(1, ntypes), function(i) {sum(2*Xsi_as[i, ]) + sum(Xsi_s[i, ]) == 1})),
+              msg = 'The transition probabilities do not some to 1. Please check!')
+  
+  tree = simulate_complete_tree(origin_time, origin_type, a, b, d, Xsi_as, Xsi_s, min_tips)
+  if (is.null(tree)) {
+    return(NULL)
+  }
+  
+  # prune dead particles
+  dead_nodes = tree %>% as_tibble %>% filter(!is.na(label) & height > 0) %>% pull(label)
+  phylogeny = drop.tip(tree, dead_nodes)
+  if (is.null(phylogeny)) {
+    print('All particle died! Try another seed.')
+    return(NULL)
+  }
+
+  # prune unsampled particles
+  tips = phylogeny@phylo$tip.label
+  sampled_tips = sample(c(TRUE, FALSE), length(tips), prob = c(rho, 1 - rho), replace = TRUE)
+  if (sum(sampled_tips) < min_tips) {
+    print('Not enough tips are sampled! Try another seed.')
+    return(NULL)
+  }
+  phylogeny = drop.tip(phylogeny, tips[!sampled_tips])
+  
+  # remove height from data for exporting tree
+  phylogeny@data = phylogeny@data %>% select(-height)
+  
+  return(phylogeny)
+}
+
+
+# Simulator of the complete Age-Dependent Branching Process
+simulate_complete_tree <- function(origin_time, origin_type, a, b, d, Xsi_as, Xsi_s, min_tips) {
+  #simulate_event <- function(id, time, type, parent_id) {}
+  
+  # initialize
+  edges = matrix(nrow = 0, ncol = 2)
+  edge_lengths = c()
+  nodes = matrix(nrow = 0, ncol = 7, 
+                 dimnames = list(NULL, c("id", "height", "type", "parent", "leftchild", "rightchild", "isTip")))
+  
+  # sample the lifetime of the first particle
+  root_edge = rgamma(1, shape = b[origin_type + 1], scale = a[origin_type + 1])
+  nodes = rbind(nodes, c(1, origin_time - root_edge, origin_type, NA, NA, NA, F))
+  events = rbind(matrix(nrow = 2, ncol = 7), nodes) # add dummy rows due to type conversions
+  event_counter = 1
+  
+  while (nrow(events) > 2) {
+    event = as.list(events[3, ]); events = events[-3, ] # look at one event
+
+    if (runif(1) < d[event$type + 1]) {
+      # particle dies
+      nodes[event$id, "isTip"] = T
+    } else {
+      # particle divides, create two new children
+      left_id = event_counter + 1
+      right_id = event_counter + 2
+      event_counter = event_counter + 2
+
+      # sample types
+      children_types = sample_types(event$type, Xsi_as, Xsi_s)
+      left_type = children_types[1]
+      right_type = children_types[2]
+
+      # sample lifetimes and add new nodes
+      left_lifetime = rgamma(1, shape = b[event$type + 1], scale = a[event$type + 1])
+      if (event$height - left_lifetime < 0) {
+        # censor lifetime, make tip
+        left_lifetime = event$height
+        nodes = rbind(nodes, c(left_id, 0, left_type, event$id, NA, NA, T), deparse.level = 0)
+      } else {
+        left_node = c(left_id, event$height - left_lifetime, left_type, event$id, NA, NA, F)
+        nodes = rbind(nodes, left_node, deparse.level = 0)
+        events = rbind(events, left_node, deparse.level = 0) # to be processed
+      }
+
+      right_lifetime = rgamma(1, shape = b[event$type + 1], scale = a[event$type + 1])
+      if (event$height - right_lifetime < 0) {
+        # censor lifetime, make tip
+        right_lifetime = event$height
+        nodes = rbind(nodes, c(right_id, 0, right_type, event$id, NA, NA, T), deparse.level = 0)
+      } else {
+        right_node = c(right_id, event$height - right_lifetime, right_type, event$id, NA, NA, F)
+        nodes = rbind(nodes, right_node, deparse.level = 0)
+        events = rbind(events, right_node, deparse.level = 0) # to be processed
+      }
+    
+      # add child relationships and append the current edges and edge lengths
+      nodes[event$id, c("leftchild", "rightchild")] = c(left_id, right_id)
+      edges = rbind(edges, c(event$id, left_id), c(event$id, right_id), deparse.level = 0)
+      edge_lengths = c(edge_lengths, left_lifetime, right_lifetime)
+      #print(events)
+    }
+  }
+  
+  # check number of tips
+  nodes = data.frame(nodes)
+  Nnode = sum(!nodes$isTip)
+  Ntip = sum(nodes$isTip)
+  if (Ntip < min_tips) {
+    print("The simulated tree has too few tips. Try another seed.")
+    return(NULL)
+  }
+  
+  # assign labels
+  nodes[which(nodes$isTip == T), 'label'] = c(1:Ntip) 
+  nodes[which(nodes$isTip == F), 'label'] = c((Ntip + 1):(Ntip + Nnode)) 
+  
+  # use labels in edge matrix
+  edges_recoded = matrix(nodes[as.double(edges), 'label'], nrow = nrow(edges), ncol = ncol(edges))
+  
+  # create phylogenetic tree
+  phylo_tree = list(edge = edges_recoded, edge.length = edge_lengths, root.edge = root_edge, 
+                    Nnode = Nnode, tip.label = as.character(nodes[which(nodes$isTip == T), 'label']))
+  class(phylo_tree) = "phylo"
+  
+  # create treedata object
+  tree = as.treedata(phylo_tree)
+  data = as_tibble(tree)
+  types = nodes %>% 
+    select(node = label, height, type) %>% 
+    mutate(type = as.factor(type)) %>%
+    arrange(node) %>%
+    as_tibble
+  tree@data = types
+  
+  return(tree)
+}
+
+  
+sample_types <- function(parent_type, Xsi_as, Xsi_s) {
+  ntypes = ncol(Xsi_s)
+  
+  cum_prob = 0
+  for (i in seq(0, ntypes - 1)) {
+    # symmetric case
+    cum_prob = cum_prob + Xsi_s[parent_type + 1, i + 1];
+    if (runif(1) < cum_prob) {
+      return(c(i, i))
+    }
+    # asymmetric case
+    cum_prob = cum_prob + 2*Xsi_as[parent_type + 1, i + 1];
+    if (runif(1) < cum_prob) {
+      if (runif(1) < 0.5) {
+        return(c(parent_type, i))
+      } else {
+        return(c(i, parent_type))
+      }
+    }
+  }
+}
+
+
+
+# Example
+origin_time = 10
+origin_type = 0
+a = c(2, 3)
+b = c(1, 2)
+d = c(0.2, 0.1)
+Xsi_as = rbind(c(0, 0.1), c(0.2, 0))
+Xsi_s = rbind(c(0.5, 0.3), c(0.1, 0.5))
+rho = 0.8
+set.seed(1)
+phylogeny = simulate_phylogeny(origin_time, origin_type, a, b, d, Xsi_as, Xsi_s, rho)
+ggtree(phylogeny) + geom_point(aes(color = type))
+write.beast.newick(phylogeny)
+#for (i in seq(1, 1000)) {
+#  simulate_phylogeny(origin_time, origin_type, a, b, d, Xsi_as, Xsi_s, rho)
+#}
+
+
+# other:
+# tree = read.beast.newick(textConnection("<newick_string>;")) # to plot tree from Java simulator
+# tree = simulate_complete_tree(origin_time, origin_type, a, b, d, Xsi_as, Xsi_s)
+# ggtree(tree) + geom_point(aes(color = type))
+
+    
+
+
+ 
+
+  
--- src/adbp/Simulator.java
+++ src/adbp/Simulator.java
@@ -2,22 +2,31 @@ package adbp;
 
 import beast.base.evolution.tree.Node;
 import beast.base.evolution.tree.Tree;
+import beast.base.evolution.tree.TreeInterface;
 import org.apache.commons.math3.distribution.GammaDistribution;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class Simulator {
-    private Random random;
+    private Random random = new Random();
     private int eventCounter = 0;
 
     public Simulator() {
-        this.random = new Random();
     }
 
-    // Main simulation method
-    public Tree simulate(double origin, int originType, double[] a, double[] b, double[] d, double[][] Xsi_as, double[][] Xsi_s) {
+    // Simulate a phylogeny from ADBP
+    public Tree simulateTree(double origin, int originType, double[] a, double[] b, double[] d, double[][] Xsi_as, double[][] Xsi_s, double rho) {
+        Tree tree = simulateCompleteTree(origin, originType, a, b, d, Xsi_as, Xsi_s);
+        tree = pruneTree(tree, rho);
+        return tree;
+    }
+
+
+    // Simulate the full Age-Dependent Branching Process
+    public Tree simulateCompleteTree(double origin, int originType, double[] a, double[] b, double[] d, double[][] Xsi_as, double[][] Xsi_s) {
 
         double rootTime = origin - sampleLifetime(a[originType], b[originType]);
 
@@ -25,7 +34,7 @@ public class Simulator {
         Node root = new Node();
         root.setNr(0);  // node number
         root.setHeight(rootTime);  // birth time (height in BEAST2)
-        setType(root, originType); // type (trait)
+        root.setMetaData("type", originType); // type (trait)
         root.metaDataString = "type=" + originType; // add string (for Newick format)
 
         // Create a Tree object to store the nodes
@@ -38,9 +47,9 @@ public class Simulator {
         // Simulate the ADBP process
         while (!events.isEmpty()) {
             Node event = events.remove(0); // look at an event
-            int nodeType = getType(event); // type of current particle
+            int nodeType = (int) event.getMetaData("type"); // type of current particle
 
-            if (random.nextDouble() >= d[nodeType]) { // birth (current particle gives rise to two children)
+            if (random.nextDouble() < 1 - d[nodeType]) { // birth (current particle gives rise to two children)
                 // sample types of children
                 int[] childrenTypes = sampleChildrenTypes(nodeType, Xsi_as, Xsi_s);
                 int leftType = childrenTypes[0];
@@ -53,7 +62,7 @@ public class Simulator {
                 // add children
                 Node leftChild = new Node();
                 leftChild.setNr(eventCounter++);
-                setType(leftChild, leftType);
+                leftChild.setMetaData("type", leftType);
                 leftChild.metaDataString = "type=" + leftType;
                 event.addChild(leftChild); // add child to node
                 tree.addNode(leftChild); // add child to tree
@@ -66,7 +75,7 @@ public class Simulator {
 
                 Node rightChild = new Node();
                 rightChild.setNr(eventCounter++);
-                setType(rightChild, rightType);
+                rightChild.setMetaData("type", rightType);
                 rightChild.metaDataString = "type=" + rightType;
                 event.addChild(rightChild); // add child to node
                 tree.addNode(rightChild); // add child to tree
@@ -84,14 +93,79 @@ public class Simulator {
         return tree;
     }
 
+    // Prune unsampled and dead particles (and any ancestors without sampled descendants)
+    public Tree pruneTree(Tree tree, double rho) {
+
+        List<Node> nodesToPrune = new ArrayList<>();
+
+        // collect leaves to prune
+        List<Node> leaves = tree.getExternalNodes();
+        for (Node node : leaves) {
+            if (node.getHeight() > 0) { // dead particle
+                nodesToPrune.add(node);
+            } else if (node.getHeight() == 0) {
+                if (random.nextDouble() < 1 - rho) { // unsampled particle
+                    nodesToPrune.add(node);
+                }
+            }
+        }
+
+        int n = nodesToPrune.size();
+        //System.out.println(n + " leaves to prune: ");
+        if (n == leaves.size()) {
+            //System.out.println("nothing remains!");
+            return null;
+        }
+        /*
+        // print the list
+        int[] leavesToPrune = new int[n];
+        for (int i = 0; i < n; i++) {
+            leavesToPrune[i] = nodesToPrune.get(i).getNr();
+        }
+        System.out.println(Arrays.toString(leavesToPrune));
+        */
+
+        // prune ancestors to reconstruct tree structure
+        while (!nodesToPrune.isEmpty()) {
+            Node node = nodesToPrune.remove(0); // take a node
+            if (node.isRoot()) {
+                return null;
+            }
+            Node parent = node.getParent();
+            parent.removeChild(node); // deconnect it from parent
+            tree.removeNode(node.getNr()); // remove from tree
+
+            if (parent.getChildCount() == 1) {
+                Node remainingChild = parent.getChild(0);
+                if (parent.isRoot()) {
+                    remainingChild.setParent(null);
+                    tree.removeNode(parent.getNr());
+                    tree.setRoot(remainingChild); // the remaining child becomes the new root
+                } else {
+                    Node grandParent = parent.getParent();
+                    grandParent.removeChild(parent);
+                    tree.removeNode(parent.getNr());
+                    grandParent.addChild(remainingChild); // reconnect the remaining child to the grandparent
+                }
+            } else if (parent.getChildCount() == 0) {
+                nodesToPrune.add(parent); // continue recursively
+            }
+            //System.out.println(tree.getNodeCount());
+        }
+
+        return tree;
+    }
+
+
     // Helper method to sample a lifetime from a Gamma distribution
-    public double sampleLifetime(double a, double b) {
+    private double sampleLifetime(double a, double b) {
         GammaDistribution gammaDist = new GammaDistribution(b, a);
         return gammaDist.sample();
     }
 
+
     // Helper method to sample offspring types
-    public int[] sampleChildrenTypes(int parentType, double[][] Xsi_as, double[][] Xsi_s) {
+    private int[] sampleChildrenTypes(int parentType, double[][] Xsi_as, double[][] Xsi_s) {
         // To Do: add matrix checks
         int ntypes = Xsi_s.length;
 
@@ -103,8 +177,8 @@ public class Simulator {
             if (random.nextDouble() < cumulativeProb) {
                 return new int[]{j, j};
             }
-            // assymmetric case
-            cumulativeProb += Xsi_as[parentType][j];
+            // asymmetric case
+            cumulativeProb += 2*Xsi_as[parentType][j];
             if (random.nextDouble() < cumulativeProb) {
                 if (random.nextDouble() < 0.5) {
                     return new int[]{parentType, j};
@@ -114,15 +188,5 @@ public class Simulator {
             }
         }
         return new int[]{-1, -1}; // in case of error
-    }
-
-    // Set the type (trait) of a node
-    private void setType(Node node, int type) {
-        node.setMetaData("type", type);  // Store the type as metadata
-    }
-
-    // Get the type (trait) of a node
-    private int getType(Node node) {
-        return (int) node.getMetaData("type");
     }
 }
