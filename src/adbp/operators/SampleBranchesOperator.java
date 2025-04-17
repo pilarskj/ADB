3d4dafb4f30e3734cd3bcdb4b87b4dd7c204e0af
pilarskj <julia.pilarski@gmail.com>
2025-04-17 16:36:26 +0200

    rename operator, check HR and topology violations (Julia)

---
+++ src/adbp/operators/SampleBranchesOperator.java
@@ -0,0 +1,164 @@
+package adbp.operators;
+
+import beast.base.core.Description;
+import beast.base.core.Input;
+import beast.base.evolution.tree.Node;
+import beast.base.evolution.tree.Tree;
+import beast.base.inference.Operator;
+import beast.base.inference.parameter.IntegerParameter;
+import beast.base.inference.parameter.RealParameter;
+import beast.base.util.Randomizer;
+
+import org.apache.commons.math3.distribution.GammaDistribution;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+IDEA:
+- joint tree & shape operator
+- makes the branch lengths more equal (and thus nodes more synchronous) for high shape, and more diverse for low shape
+- branch lengths are sampled directly from the lifetime distribution
+- the topology of the resulting trees does not change
+- optimizable parameters: window size for shape proposal, proportion of branches updated per move
+*/
+
+@Description("Operator that proposes a new shape parameter and" +
+        "samples internal branch lengths from lifetime distribution.")
+public class SampleBranchesOperator extends Operator {
+
+    public Input<Tree> treeInput =
+            new Input<>("tree", "tree", Input.Validate.REQUIRED);
+
+    public Input<IntegerParameter> shapeInput =
+            new Input<>("shape", "integer shape parameter of the ADB process", Input.Validate.REQUIRED);
+    // add RealParameter option
+
+    final public Input<Integer> windowSizeInput =
+            new Input<>("windowSize", "the size of the window both up and down", 5);
+
+    // public Input<Double> scaleFactorInput =
+    //         new Input<>("scaleFactor", "scaling factor of shape: ranges from 0 to 1 (close to zero is very large jumps, close to 1.0 is very small jumps)", 0.8);
+
+    public Input<Double> branchProportionInput =
+            new Input<>("branchProportion", "proportion of internal branches to be scaled", 0.1);
+
+    // further parameters needed (take current samples)
+    public Input<RealParameter> lifetimeInput =
+            new Input<>("lifetime", "expected lifetime parameter of the ADB process", Input.Validate.REQUIRED);
+
+    public Input<RealParameter> originInput =
+            new Input<>("origin", "time of origin of the process", Input.Validate.REQUIRED);
+
+
+    Tree tree;
+    IntegerParameter shape;
+    double branchProportion;
+    // double scaleFactor;
+    int windowSize;
+    RealParameter lifetime;
+    RealParameter origin;
+
+
+    @Override
+    public void initAndValidate() {
+        tree = treeInput.get();
+        shape = shapeInput.get();
+        branchProportion = branchProportionInput.get();
+        // scaleFactor = scaleFactorInput.get();
+        lifetime = lifetimeInput.get();
+        origin = originInput.get();
+        windowSize = windowSizeInput.get();
+    }
+
+    /* // cf. https://github.com/CompEvol/beast2/blob/master/src/beast/base/evolution/operator/ScaleOperator.java
+    protected double getScaler() {
+        return (scaleFactor + (Randomizer.nextDouble() * ((1.0 / scaleFactor) - scaleFactor)));
+    } */
+
+
+    @Override
+    public double proposal() {
+
+        double hastingsRatio = 0;
+
+        // old params
+        double C = lifetime.getValue();
+        int oldShape = shape.getValue();
+        GammaDistribution oldGammaDist = new GammaDistribution(oldShape, C/oldShape);
+
+        // new shape
+        // double scale = getScaler();
+        // int newShape = (int) (b * scale);
+        int newShape = oldShape + Randomizer.nextInt(2 * windowSize + 1) - windowSize; // use random walk
+        if (newShape < shape.getLower() || newShape > shape.getUpper()) {
+            // invalid move, can be rejected immediately
+            return Double.NEGATIVE_INFINITY;
+        }
+        if (newShape == oldShape) {
+            // this saves calculating the posterior
+            return Double.NEGATIVE_INFINITY;
+        }
+        // hastingsRatio += Math.log(scale); for RandomWalk, hR = 0
+        shape.setValue(newShape);
+        // define new branch length distribution
+        GammaDistribution newGammaDist = new GammaDistribution(newShape, C/oldShape);
+
+        // randomly select internal nodes, operate on branches connecting them to parents
+        // alternative: randomly select two tips, operate on the subtree they are in
+        int branchCount = (int) (branchProportion * (tree.getInternalNodeCount() - 1));
+        List<Node> internalNodes = tree.getInternalNodes();
+        int[] internalNodeNumbers = internalNodes.stream().mapToInt(Node::getNr).toArray();
+        Randomizer.shuffle(internalNodeNumbers);
+        int[] randomNodeNumbers = Arrays.copyOfRange(internalNodeNumbers, 0, branchCount);
+
+        // traverse nodes starting from the root, update selected branches
+        for (int i = internalNodes.size(); i > 0; i--) {
+            Node n = internalNodes.get(i - 1);
+            if (Arrays.stream(randomNodeNumbers).anyMatch(x -> x == n.getNr())) {
+                double oldBranchLength;
+                double newBranchLength = newGammaDist.sample();
+                double newHeight;
+                if (n.isRoot()) {
+                    oldBranchLength = origin.getValue() - n.getHeight();
+                    newHeight = origin.getValue() - newBranchLength;
+                } else {
+                    oldBranchLength = n.getLength();
+                    newHeight = n.getParent().getHeight() - newBranchLength;
+                }
+                /* // check whether new branch lengths fit in the process
+                if (newHeight < 0 ) { // || checkForChildrenNotOnList(randomNodeNumbers, n)
+                    return Double.NEGATIVE_INFINITY;
+                }
+                if (!Arrays.stream(randomNodeNumbers).anyMatch(x -> x == n.getLeft().getNr()) && (newHeight < n.getLeft().getHeight())) {
+                    return Double.NEGATIVE_INFINITY;
+                }
+                if (!Arrays.stream(randomNodeNumbers).anyMatch(x -> x == n.getRight().getNr()) && (newHeight < n.getRight().getHeight())) {
+                    return Double.NEGATIVE_INFINITY;
+                }*/
+                n.setHeight(newHeight);
+                // based on manipulation, add a value to hastingsRatio
+                double toCheck = Math.log(newGammaDist.density(newBranchLength)) - Math.log(oldGammaDist.density(oldBranchLength)); // or other way around?
+                //double toCheck2 = (b-1) * (Math.log(oldBranchLength) - Math.log(newBranchLength)) + (newBranchLength-oldBranchLength)/a;
+                hastingsRatio += toCheck;
+            }
+        }
+        // (post-hoc, check violations in tree topology (otherwise, newHeights of children might still be higher than parent)
+        for (int i = internalNodes.size(); i > 0; i--) {
+            Node n = internalNodes.get(i - 1);
+            if (n.getHeight() < 0 || n.getHeight() < n.getLeft().getHeight() || n.getHeight() < n.getRight().getHeight()) {
+                return Double.NEGATIVE_INFINITY;
+            }
+        }
+        
+        /* alternative: traverse the whole tree and check for negative lengths
+        // TODO isn't there a better way?
+        for (Node n : tree.getInternalNodes()) {
+            if (n.getLength() < 0) {
+                return Double.NEGATIVE_INFINITY;
+            }
+        } */
+
+        return hastingsRatio;
+    }
+}
---
+++ test/test/adbp/SampleBranchesOperatorTest.java
@@ -0,0 +1,102 @@
+package test.adbp;
+
+import adbp.GammaBranchingModel;
+import adbp.operators.SampleBranchesOperator;
+import beast.base.evolution.tree.Node;
+import beast.base.evolution.tree.Tree;
+import beast.base.evolution.tree.TreeParser;
+import beast.base.inference.parameter.IntegerParameter;
+import beast.base.inference.parameter.RealParameter;
+import beast.base.util.Randomizer;
+import org.apache.commons.math3.distribution.GammaDistribution;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SampleBranchesOperatorTest {
+
+    @Test
+    public void testSimpleCommands() throws Exception {
+
+        // define tree
+        Tree tree = new TreeParser("(((A:10,B:10):6,(C:2,D:2):14):2,(E:7,F:7):11);", true); // nice and easy (quite balanced)
+        // Tree tree = new TreeParser("(A:17,(B:14,(C:11,(D:8,(E:5,F:5):3):3):3):3);", true); // hierarchical
+        //System.out.println(tree.getInternalNodes());
+
+        /*// before manipulation
+        for (Node n : tree.getNodesAsArray()) {
+            System.out.println("Node number " + n.getNr() + ", height " + n.getHeight());
+        }*/
+
+        double origin = 20;
+        double lifetime = 5;
+        double branchProportion = 0.5;
+
+        double b = 100;
+        double a = lifetime / b;
+        GammaDistribution gammaDist = new GammaDistribution(b, a);
+
+        int branchCount = (int) (branchProportion * tree.getInternalNodeCount());
+        List<Node> internalNodes = tree.getInternalNodes();
+        int[] internalNodeNumbers = tree.getInternalNodes().stream().mapToInt(Node::getNr).toArray();
+        Randomizer.shuffle(internalNodeNumbers);
+        int[] randomNodeNumbers = Arrays.copyOfRange(internalNodeNumbers, 0, branchCount);
+        System.out.println(Arrays.toString(randomNodeNumbers));
+
+        /*List<Node> reversedNodes = tree.getInternalNodes();
+        Collections.reverse(reversedNodes);
+        for (Node n : reversedNodes) { */
+        for (int i = internalNodes.size(); i > 0; i--) {
+            Node n = internalNodes.get(i - 1);
+            if (Arrays.stream(randomNodeNumbers).anyMatch(x -> x == n.getNr())) {
+                double newBranchLength = gammaDist.sample();
+                double newHeight;
+                if (n.isRoot()) {
+                    newHeight = origin - newBranchLength;
+                } else {
+                    newHeight = n.getParent().getHeight() - newBranchLength;
+                }
+                // check whether new branch lengths fit in the process
+                if (newHeight < 0) {
+                    System.out.println("order violated");
+                }
+                if (!Arrays.stream(randomNodeNumbers).anyMatch(x -> x == n.getLeft().getNr()) && (newHeight < n.getLeft().getHeight())) {
+                    System.out.println("order violated");
+                }
+                if (!Arrays.stream(randomNodeNumbers).anyMatch(x -> x == n.getRight().getNr()) && (newHeight < n.getRight().getHeight())) {
+                    System.out.println("order violated");
+                }
+                n.setHeight(newHeight);
+            }
+        }
+
+        // after manipulation
+        for (Node n : tree.getNodesAsArray()) {
+            System.out.println("Node number " + n.getNr() + ", height " + n.getHeight());
+        }
+        System.out.println(tree);
+    }
+
+    @Test
+    public void testSampleBranches() throws Exception {
+
+        // define tree
+        Tree tree = new TreeParser("(((A:10,B:10):6,(C:2,D:2):14):2,(E:7,F:7):11);", true); // nice and easy (quite balanced)
+        // Tree tree = new TreeParser("(A:17,(B:14,(C:11,(D:8,(E:5,F:5):3):3):3):3);", true); // hierarchical
+        //System.out.println(tree.getInternalNodes());
+        System.out.println(tree);
+
+        SampleBranchesOperator op = new SampleBranchesOperator();
+        op.setInputValue("tree", tree);
+        op.setInputValue("lifetime", new RealParameter("5"));
+        op.setInputValue("shape", new IntegerParameter("10"));
+        op.setInputValue("origin", new RealParameter("20"));
+
+        double hr = op.proposal();
+        System.out.println(hr);
+        System.out.println(tree);
+    }
+
+}
