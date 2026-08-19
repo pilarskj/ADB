8d5d41d3b6c30d9a85a6fa3d9fa08a9a2640a0f7
pilarskj <julia.pilarski@gmail.com>
2026-08-19 11:56:50 +0200

    joint event sampler - v3 UpDown

--- src/adb/operators/JointEventSampler.java
+++ src/adb/operators/JointEventSampler.java
@@ -2,6 +2,7 @@ package adb.operators;
 
 import adb.tree.AnnotatedNode;
 import beast.base.core.Input;
+import beast.base.core.Log;
 import beast.base.evolution.tree.Tree;
 import beast.base.inference.parameter.RealParameter;
 import beast.base.inference.util.InputUtil;
@@ -10,32 +11,35 @@ import beast.base.util.Randomizer;
 import java.util.ArrayList;
 import java.util.List;
 
-// chains lifetime scaling and resampling hidden events together
-// currently partly redundant with AnnotatedScale
+// chains parameter scaling and resampling hidden events together
+// extends UpDownOperator, modified AnnotatedScale
 public class JointEventSampler extends AnnotatedTreeOperator {
 
-    public Input<List<RealParameter>> parametersInput =
-            new Input<>("parameter",
-                    "scale this scalar parameter",
-                    new ArrayList<RealParameter>());
+    public Input<List<RealParameter>> upInput = new Input<>("up",
+            "parameters to scale upwards", new ArrayList<>());
 
-    public Input<List<RealParameter>> parametersInverseInput =
-            new Input<>("parameterInverse",
-                    "scale this scalar parameter inversely",
-                    new ArrayList<RealParameter>());
+    public Input<List<RealParameter>> downInput = new Input<>("down",
+            "parameters to scale downwards", new ArrayList<>());
+
+    public Input<Boolean> elementWiseInput = new Input<>("elementWise", "flag to indicate that the scaling is applied to a random index in multivariate parameters (default false)", false);
 
     public Input<Double> scaleFactorInput = new Input<>("scaleFactor",
-            "for scaling real parameter, is restricted to the range [1/scaleFactor, scaleFactor]", 0.75);
+            "magnitude factor used for scaling", 0.75);
 
     public Input<Boolean> optimiseInput = new Input<>("optimise",
             "flag to indicate that the scale factor is automatically changed in order to achieve a good acceptance rate (default true)", true);
 
     double scaleFactor;
 
+
     @Override
     public void initAndValidate() {
         super.initAndValidate();
         scaleFactor = scaleFactorInput.get();
+        // sanity check
+        if (upInput.get().size() + downInput.get().size() == 0) {
+            Log.warning.println("WARNING: At least one up or down item must be specified");
+        }
     }
 
 
@@ -58,10 +62,11 @@ public class JointEventSampler extends AnnotatedTreeOperator {
         double logf = Math.log(f);
         double logHR = -2 * logf;
 
-        // scale parameters
-        for (int pidx=0; pidx<parametersInput.get().size(); pidx++) {
-            RealParameter param = parametersInput.get().get(pidx);
-            for (int i=0; i<param.getDimension(); i++) {
+        // TODO: add option to scale just one element
+        // scale parameters upwards
+        for (int pidx = 0; pidx < upInput.get().size(); pidx++) {
+            RealParameter param = upInput.get().get(pidx);
+            for (int i = 0; i < param.getDimension(); i++) {
                 double oldValue = param.getValue(i);
                 double newValue = oldValue * f;
                 if (newValue < param.getLower() || newValue > param.getUpper())
@@ -72,9 +77,9 @@ public class JointEventSampler extends AnnotatedTreeOperator {
             }
         }
 
-        // scale parameters inversely
-        for (int pidx=0; pidx<parametersInverseInput.get().size(); pidx++) {
-            RealParameter param = parametersInverseInput.get().get(pidx);
+        // scale parameters downwards
+        for (int pidx = 0; pidx < downInput.get().size(); pidx++) {
+            RealParameter param = downInput.get().get(pidx);
             for (int i=0; i<param.getDimension(); i++) {
                 double oldValue = param.getValue(i);
                 double newValue = oldValue/f;
@@ -95,5 +100,5 @@ public class JointEventSampler extends AnnotatedTreeOperator {
         return logHR;
     }
 
-    // TODO: add optimisation of scaling factor?
+    // TODO: add optimisation of scaling factor
 }
