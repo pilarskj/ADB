c4286dfed389c32ac064a79edcd044285f65aaec
pilarskj <julia.pilarski@gmail.com>
2026-08-20 16:13:40 +0200

    event sampling on fraction of branches, optimization

--- src/adb/operators/EventSampler.java
+++ src/adb/operators/EventSampler.java
@@ -5,6 +5,7 @@ import beast.base.core.Input;
 import beast.base.evolution.tree.Node;
 import beast.base.evolution.tree.Tree;
 import beast.base.inference.util.InputUtil;
+import beast.base.util.Randomizer;
 
 
 public class EventSampler extends AnnotatedTreeOperator {
@@ -12,13 +13,17 @@ public class EventSampler extends AnnotatedTreeOperator {
     public Input<Boolean> drawEventCountInput = new Input<>("drawEventCount",
             "draw or fix the number of events along internal branches (default true)", true);
 
-    boolean drawEventCount;
+    public Input<Double> proportionBranchesInput = new Input<>("proportionBranches",
+            "proportion of branches to sample events on (default 0.1)", 0.1);
 
+    boolean drawEventCount;
+    double proportionBranches;
 
     @Override
     public void initAndValidate() {
         super.initAndValidate();
         drawEventCount = drawEventCountInput.get();
+        proportionBranches = proportionBranchesInput.get();
     }
 
 
@@ -29,11 +34,15 @@ public class EventSampler extends AnnotatedTreeOperator {
         double oldBranchProb, newBranchProb;
         double logHR = 0;
 
+        double u;
         // TODO: extend to multi-type version
         for (Node node : tree.getNodesAsArray()) {
-            oldBranchProb = getBranchProbability((AnnotatedNode)node, drawEventCount);
-            newBranchProb = resampleEvents((AnnotatedNode)node, drawEventCount);
-            logHR += (oldBranchProb - newBranchProb);
+            u = Randomizer.nextDouble();
+            if (u < proportionBranches) {
+                oldBranchProb = getBranchProbability((AnnotatedNode)node, drawEventCount);
+                newBranchProb = resampleEvents((AnnotatedNode)node, drawEventCount);
+                logHR += (oldBranchProb - newBranchProb);
+            }
         }
 
         // for testing
--- src/adb/operators/JointEventSampler.java
+++ src/adb/operators/JointEventSampler.java
@@ -8,6 +8,7 @@ import beast.base.inference.parameter.RealParameter;
 import beast.base.inference.util.InputUtil;
 import beast.base.util.Randomizer;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -101,4 +102,45 @@ public class JointEventSampler extends AnnotatedTreeOperator {
     }
 
     // TODO: add optimisation of scaling factor
+    /**
+     * automatic parameter tuning *
+     */
+    @Override
+    public void optimize(double logAlpha) {
+        if (optimiseInput.get()) {
+            double delta = calcDelta(logAlpha);
+            delta += Math.log(1.0 / scaleFactor - 1.0);
+            setCoercableParameterValue(1.0 / (Math.exp(delta) + 1.0));
+        }
+    }
+
+    @Override
+    public double getCoercableParameterValue() {
+        return scaleFactor;
+    }
+
+    @Override
+    public void setCoercableParameterValue(double value) {
+        scaleFactor = Math.max(Math.min(value, 1.0), 0.0);
+    }
+
+    @Override
+    public String getPerformanceSuggestion() {
+        final double prob = m_nNrAccepted / (m_nNrAccepted + m_nNrRejected + 0.0);
+        final double targetProb = getTargetAcceptanceProbability();
+
+        double ratio = prob / targetProb;
+        if (ratio > 2.0) ratio = 2.0;
+        if (ratio < 0.5) ratio = 0.5;
+
+        // new scale factor
+        final double sf = Math.pow(scaleFactor, ratio);
+
+        final DecimalFormat formatter = new DecimalFormat("#.###");
+        if (prob < 0.10) {
+            return "Try setting scaleFactor to about " + formatter.format(sf);
+        } else if (prob > 0.40) {
+            return "Try setting scaleFactor to about " + formatter.format(sf);
+        } else return "";
+    }
 }
